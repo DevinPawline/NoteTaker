b8d14c4186cbde343a3e464c71a9988dc0ed286d
Devin Pawline <devinpawline@outlook.com>
2021-07-15 13:13:54 -0400

    started server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json")
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
