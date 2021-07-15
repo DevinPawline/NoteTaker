86f5712f106dde498a54bae793a07143b0679459
Devin Pawline <devinpawline@outlook.com>
2021-07-15 13:15:09 -0400

    added comments to what i've done in server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+// Dependecies 
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db.json")
 
-
+// Express configuration
 const app = express();
 
+// Sets initial port
 const PORT = process.env.PORT || 3000;
 
-
+// Sets up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
